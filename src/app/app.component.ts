3cf6060d7091cc0eefbfe0b666bbbb6ac6da3178
Roman Blumer <roblumination@gmail.com>
2022-04-14 18:52:24 +0300

    Add hover effect on contols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,28 +26,34 @@ export class AppComponent implements OnInit {
     {
       name: 'VSCode Anventures',
       year: 1998,
-      money: 300,
+      money: 10,
       picture: 'film-3.jpg',
     },
     { name: 'QA and Devs', year: 998, money: 100000, picture: 'film-4.jpg' },
     {
       name: 'Master or Main?',
-      year: 3001,
-      money: 31300,
+      year: 2023,
+      money: 3130041241,
       picture: 'film-5.jpg',
     },
     {
       name: 'Mining on CPU',
-      year: 1995,
+      year: 2015,
       money: 435000,
       picture: 'film-6.jpg',
     },
     { name: 'C++ course', year: 2002, money: 513531, picture: 'film-8.jpg' },
     {
       name: 'Ruby on rails',
-      year: 9999,
+      year: 2013,
       money: 4124135,
       picture: 'film-7.jpg',
+    },
+    {
+      name: 'Pair Programming',
+      year: 1993,
+      money: 4124135,
+      picture: 'film-9.jpg',
     },
   ];
 
