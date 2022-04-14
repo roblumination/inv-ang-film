38b963fd93b1f23457908fb8397114e4736d1485
Roman Blumer <roblumination@gmail.com>
2022-04-14 19:48:19 +0300

    Update image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
     {
       name: 'Master or Main?',
       year: 2023,
-      money: 3130041241,
+      money: 313004124142,
       picture: 'film-5.jpg',
     },
     {
@@ -42,10 +42,10 @@ export class AppComponent implements OnInit {
       money: 435000,
       picture: 'film-6.jpg',
     },
-    { name: 'C++ course', year: 2002, money: 513531, picture: 'film-8.jpg' },
+    { name: 'C++ course', year: 2012, money: 513531, picture: 'film-8.jpg' },
     {
       name: 'Ruby on rails',
-      year: 2013,
+      year: 1984,
       money: 4124135,
       picture: 'film-7.jpg',
     },
